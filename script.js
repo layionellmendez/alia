24da57da125d638a4f03de30ceff643a2701e6aa
Layionell Stwart Méndez Jiménez <67165194+layionellmendez@users.noreply.github.com>
2023-02-02 05:26:21 -0600

    update code

--- script.js
+++ script.js
@@ -1,117 +1,40 @@
-// Cerebro creado por Layionell Stwart Méndez Jiménez
-
+const win = window;
 document.addEventListener("DOMContentLoaded", function() {
-   if (!annyang) {
-      return alert("C mamut, tu navegador no soporta el reconocimiento de voz.");
-   }
-   
-   if (annyang) {
-      annyang.setLanguage("es-CR");
-      var comandos = {
-         
-         /*
-         
-         
-         "": () => {
-           window.open(``);
-         },
-           
-           
-         */
-         
-         // Buscar en Google
-            
-         "busca *direccionG1": direccionG1 => {
-            window.open(`https://www.google.com/search?q=${direccionG1}`);
-         },
-         "alia busca *direccionG2": direccionG2 => {
-            window.open(`https://www.google.com/search?q=${direccionG2}`);
-         },
-         
-         
-         
-         // Buscar en YouTube
-         
-         "abre *direccionYT1 en youtube": direccionYT1 => {
-            window.open(`https://www.youtube.com/results?search_query=${direccionYT1}`);
-         },
-         "alia abre *direccionYT2 en youtube": direccionYT2 => {
-            window.open(`https://www.youtube.com/results?search_query=${direccionYT2}`);
-         },
-         
-         // Links Predeterminados
-         
-         "abre youtube": () => {
-           window.open(`https://www.youtube.com/`);
-         },
-         "alia abre youtube": () => {
-           window.open(`https://www.youtube.com/`);
-         },
-         
+  if (!annyang) {
+    return alert("C mamut, tu navegador no soporta el reconocimiento de voz.");
+  }
 
-         "abre google": () => {
-           window.open(`https://www.google.com/`);
-         },
-         "alia abre google": () => {
-           window.open(`https://www.google.com/`);
-         },
-         
-         
-         "abre facebook": () => {
-           window.open(`https://www.facebook.com/`);
-         },
-         "alia abre facebook": () => {
-           window.open(`https://www.facebook.com/`);
-         },
-         
-         
-         "abre twitter": () => {
-           window.open(`https://www.twitter.com/`);
-         },
-         "alia abre twitter": () => {
-           window.open(`https://www.twitter.com/`);
-         },
-         
-         
-         "abre pinterest": () => {
-           window.open(`https://www.pinterest.com/`);
-         },
-         "alia abre pinterest": () => {
-           window.open(`https://www.pinterest.com/`);
-         },
-         
-         "abre instagram": () => {
-           window.open(`https://www.instagram.com/`);
-         },
-         "alia abre instagram": () => {
-           window.open(`https://www.instagram.com/`);
-         },
-         
-         "abre wikipedia": () => {
-           window.open(`https://www.wikipedia.com/`);
-         },
-         "alia abre wikipedia": () => {
-           window.open(`https://www.wikipedia.com/`);
-         },
-         
-         
-         "abre codepen": () => {
-           window.open(`https://codepen.io/`);
-         },
-         "alia abre codepen": () => {
-           window.open(`https://codepen.io/`);
-         },
-         
-         "dime la hora": () => {
-           window.open(`https://www.google.com/search?q=hora&rlz=1CATATK_enCR966&sxsrf=AOaemvIfIzlgNOctKrlsYIJ6Tm3PmIzomg%3A1634138306612&ei=wvhmYfruJJSDwbkPubGckAI&ved=0ahUKEwj6153K18fzAhWUQTABHbkYByIQ4dUDCA4&uact=5&oq=hora&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAELEDEEMyBAgAEEMyBAgAEEMyCAgAEIAEELEDMgUIABCABDIICAAQgAQQsQMyBQgAEIAEMggIABCABBCxAzIICAAQgAQQsQMyBQgAEIAEOgcIIxDqAhAnOgQIIxAnOggILhCxAxCDAToLCC4QgAQQsQMQgwE6CwgAEIAEELEDEIMBOgoILhDHARCjAhBDOgoIABCxAxCDARBDSgQIQRgAUOs6WJxDYPVGaAFwAngAgAH8AYgBpAaSAQUwLjIuMpgBAKABAbABCsABAQ&sclient=gws-wiz/`);
-         },
-         "alia dime la hora": () => {
-           window.open(`https://www.google.com/search?q=hora&rlz=1CATATK_enCR966&sxsrf=AOaemvIfIzlgNOctKrlsYIJ6Tm3PmIzomg%3A1634138306612&ei=wvhmYfruJJSDwbkPubGckAI&ved=0ahUKEwj6153K18fzAhWUQTABHbkYByIQ4dUDCA4&uact=5&oq=hora&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAELEDEEMyBAgAEEMyBAgAEEMyCAgAEIAEELEDMgUIABCABDIICAAQgAQQsQMyBQgAEIAEMggIABCABBCxAzIICAAQgAQQsQMyBQgAEIAEOgcIIxDqAhAnOgQIIxAnOggILhCxAxCDAToLCC4QgAQQsQMQgwE6CwgAEIAEELEDEIMBOgoILhDHARCjAhBDOgoIABCxAxCDARBDSgQIQRgAUOs6WJxDYPVGaAFwAngAgAH8AYgBpAaSAQUwLjIuMpgBAKABAbABCsABAQ&sclient=gws-wiz/`);
-         },
-      }
-         
-   };
-      annyang.addCommands(comandos);
-      annyang.start()
- 
+  if (annyang) {
+    annyang.setLanguage("es-CR");
+    const comandos = {
+      "busca en Google *query": (query) => {
+        win.open(`https://www.google.com/search?q=${query}`);
+      },
+      "busca en YouTube *query": (query) => {
+        win.open(`https://www.youtube.com/results?search_query=${query}`);
+      },
+      "dime la hora": () => {
+        win.open(`https://www.google.com/search?q=hora`);
+      },
+    };
+
+    const linksPredeterminados = [
+      {nombre: "youtube", url: "https://www.youtube.com/"},
+      {nombre: "google", url: "https://www.google.com/"},
+      {nombre: "facebook", url: "https://www.facebook.com/"},
+      {nombre: "twitter", url: "https://www.twitter.com/"},
+      {nombre: "pinterest", url: "https://www.pinterest.com/"},
+      {nombre: "instagram", url: "https://www.instagram.com/"},
+      {nombre: "wikipedia", url: "https://www.wikipedia.com/"},
+      {nombre: "codepen", url: "https://codepen.io/"},
+    ];
+
+    linksPredeterminados.forEach(({nombre, url}) => {
+      comandos[`abre ${nombre}`] = () => { win.open(url) };
+      comandos[`alia abre ${nombre}`] = () => { win.open(url) };
+    });
+
+    annyang.addCommands(comandos);
+    annyang.start();
+  }
 });
